0f2c28d1b4aa43753a898f7d611ea3699a1e3f75
beemfx <beem.media@outlook.com>
2015-01-20 16:44:13 -0800

    [SG] Finalize the untested renderer.

--- Source/Renderer2/RendererImage.h
+++ Source/Renderer2/RendererImage.h
@@ -7,11 +7,57 @@
 ////////////////////////////////////////////////////////////////////////////
 #pragma once
 
+
+union sgColor
+{
+	unsigned __int32 Color;
+	struct
+	{
+		unsigned __int8 a;
+		unsigned __int8 r;
+		unsigned __int8 g;
+		unsigned __int8 b;
+	};
+};
+
+enum RENDERER_IMAGE_T
+{
+	RENDERER_IMAGE_UNK,
+	RENDERER_IMAGE_BITMAP,
+	RENDERER_IMAGE_COLOR,
+};
+
+struct sgRendererImageCreateParms
+{
+	RENDERER_IMAGE_T Type;
+	int              Width;
+	int              Height;
+	sgColor          Color;
+	int              BmX;
+	int              BmY;
+	int              BmWidth;
+	int              BmHeight;
+	void*            Bitmap; //(HBITMAP)
+	size_t           BitmapSize;
+};
+
+struct sgRendererData
+{
+	struct IDirectDraw7*        Dd;
+	struct IDirectDrawSurface7* BackSurface;
+};
+
 class SgRendererImage
 {
 public:
-	SgRendererImage();
+	SgRendererImage( const sgRendererImageCreateParms* CreateParms , const sgRendererData* RendererData );
 	~SgRendererImage();
+
+	void Draw( int x , int y );
+	void Restore();
+private:
+	void CreateBitmap();
+	void CreateColor();
 private:
 	struct sgData;
 	sgData* m_D;
