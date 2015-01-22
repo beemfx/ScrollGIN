4cd034f70b3778e2182cdeb9764f70be6c068809
beemfx <beem.media@outlook.com>
2015-01-22 12:14:40 -0800

    [SG] Cleanup some rendering stuff.

--- Source/Renderer2/RendererImage.h
+++ Source/Renderer2/RendererImage.h
@@ -7,24 +7,10 @@
 ////////////////////////////////////////////////////////////////////////////
 #pragma once
 
-
-union sgColor
-{
-	unsigned __int32 Color;
-	struct
-	{
-		unsigned __int8 a;
-		unsigned __int8 r;
-		unsigned __int8 g;
-		unsigned __int8 b;
-	};
-};
-
 enum RENDERER_IMAGE_T
 {
 	RENDERER_IMAGE_UNK,
 	RENDERER_IMAGE_BITMAP,
-	RENDERER_IMAGE_COLOR,
 };
 
 static const int IMAGE_CREATE_DEFAULT = -1;
@@ -34,7 +20,6 @@ struct sgRendererImageCreateParms
 	RENDERER_IMAGE_T Type;
 	int              Width;
 	int              Height;
-	sgColor          Color;
 	int              BmX;
 	int              BmY;
 	int              BmWidth;
@@ -62,7 +47,6 @@ public:
 	void Restore();
 private:
 	void CreateBitmap();
-	void CreateColor();
 private:
 	struct sgData;
 	sgData* m_D;
